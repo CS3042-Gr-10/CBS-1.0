905d8fb4aa607a576e3a8f736742262ec318f057
Devin Yasith De Silva <57395782+devindesilva@users.noreply.github.com>
2020-12-05 11:09:44 +0530

    Initial Routing Fixes #3

---
+++ app/routes/Employee.route.js
@@ -0,0 +1,20 @@
+const userService = require('../services/user.service');
+var schema = require('../schema/userValidationSchema.json')
+var iValidator = require('../../common/iValidator');
+var errorCode = require('../../common/error-code');
+var errorMessage = require('../../common/error-methods');
+var mail = require('./../../common/mailer.js');
+
+
+function init(router) {
+    router.route('/user')
+        .get(getAllUsers)
+        .post(addUser);
+    router.route('/user/:id')
+        .get(getUserById)
+        .delete(deleteUser)
+        .put(updateUser);
+}
+
+
+module.exports.init = init;
