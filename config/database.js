68486b8f1627ff0e180a2f551a52ece808fb5a9e
nipdep <nipun1deelaka@gmail.com>
2020-12-06 13:10:05 +0530

    unknown changes

--- config/database.js
+++ config/database.js
@@ -7,6 +7,7 @@ module.exports = mysql.createPool({
     user: 'root',
     password: 'admin',
     ssl: {
+        // cs: fs.readFileSync('./documents/ssl-keys-8/server-ca-1.pem')
         ca: fs.readFileSync('./documents/ssl-keys-8/server-ca.pem'),
         key: fs.readFileSync('./documents/ssl-keys-8/client-key.pem'),
         cert: fs.readFileSync('./documents/ssl-keys-8/client-cert.pem')
--- config/db-function.js
+++ config/db-function.js
@@ -24,3 +24,5 @@ module.exports = {
     connectionCheck:connectionCheck(),
     connectionRelease:connectionRelease()   
 }
+
+
