25b6924c99999c6630d307d29d4e6f3778f85fdd
Devin <devin.18@cse.mrt.ac.lk>
2020-12-09 19:35:23 +0530

    login page routing done#3

--- app/routes/BankManager.route.js
+++ app/routes/BankManager.route.js
@@ -6,12 +6,12 @@ const errorCode = require('../../common/error-code');
 const errortype = require('../../common/error-type');
 const errorMessage = require('../../common/error-methods');
 const mail = require('./../../common/mailer.js');
-const errors = require('../app/routes/error.route')
-
+//const errors = require('../app/routes/error.route')
+const GeneralError = errortype.RedirectGeneralError;
 
 function init(router) {
     router.route('/BankManager')
-        .get(errortype.RedirectGeneralError);
+        .get(GeneralError);
     router.route('/BankManager/:id')
         .get(getUserById)
         .delete(deleteUser)
