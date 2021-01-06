4b3aec1678faee45574174308593328e947d3e21
nipdep <nipun1deelaka@gmail.com>
2021-01-06 10:12:34 +0530

    Edit: schema.sql > complete upto transactions

--- documents/sql-script/schema.sql
+++ documents/sql-script/schema.sql
@@ -93,61 +93,95 @@ CREATE TABLE `Organization` (
   FOREIGN KEY (branch_id) REFERENCES Branch (branch_id)
 );
 
--------------------------------------------------------------------------------------------------------------------------------------------------
+CREATE TABLE `Account` (
+  `acc_id` INT(32) not NULL AUTO_INCREMENT,
+  `branch_id` int(5) not NULL,
+  `manager_id` INT(11),
+  `user_id` INT(11) not NULL,
+  `acc_type` int(2) not NULL,
+  `created date` Date not NULL,
+  PRIMARY KEY (`acc_id`),
+  FOREIGN KEY (branch_id) REFERENCES Branch (branch_id),
+  FOREIGN KEY (manager_id) REFERENCES Employee (emp_id),
+  FOREIGN KEY (user_id) REFERENCES AccountOwner (owner_id)
+);
+
+CREATE TABLE `SavingAccoutPlan` (
+  `acc_plan_id` int(8) not NULL AUTO_INCREMENT,
+  `name` varchar(25) not NULL,
+  `min_age` int(8),
+  `max_age` int(8),
+  `interest_rate` int(8),
+  `min_balance_to_open` int(8),
+  `maximum_num_wt` int(3),
+  PRIMARY KEY (`acc_plan_id`)
+);
+
+CREATE TABLE `SavingAccount` (
+  `saving_acc_id` INT(32) not NULL,
+  `acc_plan_id` int(4) not NULL,
+  `acc_balance` Numeric(20,2),
+  PRIMARY KEY (`saving_acc_id`),
+  FOREIGN KEY (saving_acc_id) REFERENCES Account (acc_id),
+  FOREIGN KEY (acc_plan_id) REFERENCES SavingAccoutPlan (acc_plan_id)
+);
 
 CREATE TABLE `Current Deposit` (
-  `crr_acc_id` int(32),
-  `remaining_balance` int(64),
-  `state()` int(8),
-  `` <type>,
-  `` <type>,
-  `` <type>,
-  KEY `PK,FK ` (`crr_acc_id`)
+  `current_acc_id` INT(32) not NULL,
+  `remaining_balance` Numeric(20,2),
+  `state` int(4) not NULL,
+  PRIMARY KEY (`current_acc_id`),
+  FOREIGN KEY (current_acc_id) REFERENCES Account (acc_id)
+);
+
+CREATE TABLE `FDAccountPlan` (
+  `fd_plan_id` int(8) not NULL AUTO_INCREMENT,
+  `name` varchar(25) not NULL,
+  `duration` int(8),
+  `interest_rate_per_mon` Numeric(4,2),
+  PRIMARY KEY (`fd_plan_id`)
+);
+
+CREATE TABLE `FixedDeposit` (
+  `fd_id` INT(32) not NULL AUTO_INCREMENT,
+  `cusotmer_id` int(16) not NULL,
+  `acc_plan_id` int(8) not NULL,
+  `sv_acc_id` int(32) not NULL,
+  `branch_id` int(5) not NULL,
+  `opened_date` Date,
+  `balance` Numeric(20,2),
+  PRIMARY KEY (`fd_id`),
+  FOREIGN KEY (cusotmer_id) REFERENCES AccountOwner (owner_id),
+  FOREIGN KEY (acc_plan_id) REFERENCES  FDAccountPlan (fd_plan_id),
+  FOREIGN KEY (sv_acc_id) REFERENCES SavingAccount (saving_acc_id),
+  FOREIGN KEY (branch_id) REFERENCES Branch (branch_id)
 );
 
 CREATE TABLE `Loan` (
-  `loan_id` int(32),
-  `accepted_date` datetime,
-  `loan_init_type` Bool,
-  `customer_id` int(16),
-  `loaned_amount` int(16),
-  `state()` int(8),
-  `sv_acc_id` int(32),
-  `loan_interrest_rate` int(8),
-  `#mon_paid` int(8),
-  `branch_id` int(8),
-  `requested_date` datetime,
-  `` <type>,
-  KEY `PK ` (`loan_id`)
+  `loan_id` INT(32) not NULL AUTO_INCREMENT,
+  `requested_date` Date,
+  `accepted_date` Date,
+  `loan_init_type` int(2) not NULL,
+  `customer_id` int(16) not NULL,
+  `loaned_amount` Numeric(20,2) not NULL,
+  `state` int(4) not NULL,
+  `sv_acc_id` int(32) not NULL,
+  `loan_interrest_rate` Numeric(4,2),
+  `num_mon_paid` int(8) not NULL Default 0,
+  `branch_id` int(5) not NULL,
+  PRIMARY KEY (`loan_id`),
+  FOREIGN KEY (customer_id) REFERENCES AccountOwner (owner_id),
+  FOREIGN KEY (sv_acc_id) REFERENCES SavingAccount (saving_acc_id),
+  FOREIGN KEY (branch_id) REFERENCES Branch (branch_id)
 );
+-------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 CREATE TABLE `Deposit` (
   `trans_id` int(64),
   `depositer_type` bool,
   `depositer_id` int(64),
   PRIMARY KEY (`trans_id`)
-);
-
-CREATE TABLE `Account` (
-  `sv_acc_id` int(32),
-  `branch_id` int(8),
-  `manager_id` int(16),
-  `customer_type` bool,
-  `user_id` int(16),
-  `acc_type` int(8),
-  `created date` datetime,
-  KEY `PK,` (`sv_acc_id`)
-);
-
-CREATE TABLE `Saving Accout Plan` (
-  `acc_plan_id` int(8),
-  `min_age` int(8),
-  `max_age` int(8),
-  `interest_rate` int(8),
-  `min_balance_to_open` int(8),
-  `name` varchar(25),
-  `maximum_num_wt` int(3),
-  PRIMARY KEY (`acc_plan_id`)
 );
 
 CREATE TABLE `Transfer` (
@@ -157,15 +191,6 @@ CREATE TABLE `Transfer` (
   PRIMARY KEY (`trans_id`)
 );
 
-CREATE TABLE `Organization` (
-  `org_id` int(8),
-  `name` varchar(25),
-  `branch_id` int(8),
-  `created_date` timestamp,
-  `{customer_id}` int(16),
-  PRIMARY KEY (`org_id`)
-);
-
 CREATE TABLE `Withdrawl` (
   `trans_id` int(64),
   `acc_id` int(32),
@@ -173,29 +198,6 @@ CREATE TABLE `Withdrawl` (
   `withdrawer_id` int(64),
   PRIMARY KEY (`trans_id`)
 );
-
-
-CREATE TABLE `Saving Account ` (
-  `saving_acc_id` int(8),
-  `acc_plan_id` varchar(25),
-  `acc_balance` int(16),
-  `` <type>,
-  `` <type>,
-  PRIMARY KEY (`saving_acc_id`)
-);
-
-CREATE TABLE `FD Account Plan` (
-  `fd_plan_id` int(8),
-  `name` varchar(25),
-  `duration` int(8),
-  `interest_rate_per_mon` int(8),
-  `` <type>,
-  `` <type>,
-  `` <type>,
-  PRIMARY KEY (`fd_plan_id`)
-);
-
-
 
 
 CREATE TABLE `Check` (
@@ -208,18 +210,6 @@ CREATE TABLE `Check` (
   PRIMARY KEY (`check_id`)
 );
 
-CREATE TABLE `Fixed Deposit` (
-  `fd_id` int(32),
-  `cusotmer_id` int(16),
-  `acc_plan_id` int(8),
-  `sv_acc_id` int(32),
-  `opened_date` datetime,
-  `branch_id` int(8),
-  `` <type>,
-  `` <type>,
-  `` <type>,
-  PRIMARY KEY (`fd_id`)
-);
 
 CREATE TABLE `Transaction` (
   `trans_id` int(64),
