d36d519303ee79d8a399af805c2a7dee85ba4375
nipdep <nipun1deelaka@gmail.com>
2021-01-30 16:55:31 +0530

    Edit : Customer.service

--- app/models/Customer.model.js
+++ app/models/Customer.model.js
@@ -2,6 +2,51 @@
 const db = require('../config/database');
 var dbFunc = require('../config/db-function');
 
+function getCustomerDetails(id) {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT * from Customer where customer_id = ?',id, (error, rows, fields) => {
+            if (!!error) {
+                dbFunc.connectionRelease;
+                reject(false);
+            } else {
+
+                dbFunc.connectionRelease;
+                resolve(rows[0]);
+            }
+        });
+    });
+}
+
+function getCustomerSavAccDetail(id) {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT acc_id, branch_id, acc_type, created_date from Account where user_id = ?',id, (error, rows, fields) => {
+            if (!!error) {
+                dbFunc.connectionRelease;
+                reject(false);
+            } else {
+
+                dbFunc.connectionRelease;
+                resolve(rows[0]);
+            }
+        });
+    });
+}
+
+function getCustomerDetailsByNIC(nic) {
+  return new Promise((resolve, reject) => {
+      db.query('SELECT * from Customer where NIC = ?',nic, (error, rows, fields) => {
+          if (!!error) {
+              dbFunc.connectionRelease;
+              reject(false);
+          } else {
+
+              dbFunc.connectionRelease;
+              resolve(rows[0]);
+          }
+      });
+  });
+}
+
 function addCustomer(acc) {
 
     //TODO: set "acc" attribute appropriate to the data passing -- checkout ../document/sql_scripts/add_customer.sql 
--- app/services/Customer.service.js
+++ app/services/Customer.service.js
@@ -6,6 +6,7 @@ const dbFunc = require('../../config/db-function');
 
 const CustomerModel = require("../models/Customer.model");
 
+/*
 function storeCustomer(customer){
     try {  
         let results = []
@@ -63,67 +64,6 @@ function storeCustomer(customer){
         return Promise.reject(error)
       }
 }
+*/
 
 
-function getCustomerDetails(id) {
-    return new Promise((resolve, reject) => {
-        db.query('SELECT * from Customer where customer_id = ?',id, (error, rows, fields) => {
-            if (!!error) {
-                dbFunc.connectionRelease;
-                reject(false);
-            } else {
-
-                dbFunc.connectionRelease;
-                resolve(rows[0]);
-            }
-        });
-    });
-}
-
-function getCustomerSavAccDetail(id) {
-    return new Promise((resolve, reject) => {
-        db.query('SELECT acc_id, branch_id, acc_type, created_date from Account where user_id = ?',id, (error, rows, fields) => {
-            if (!!error) {
-                dbFunc.connectionRelease;
-                reject(false);
-            } else {
-
-                dbFunc.connectionRelease;
-                resolve(rows[0]);
-            }
-        });
-    });
-}
-
-function getCustomerDetailsByNIC(nic) {
-  return new Promise((resolve, reject) => {
-      db.query('SELECT * from Customer where NIC = ?',nic, (error, rows, fields) => {
-          if (!!error) {
-              dbFunc.connectionRelease;
-              reject(false);
-          } else {
-
-              dbFunc.connectionRelease;
-              resolve(rows[0]);
-          }
-      });
-  });
-}
-
-function addCustomer(customer) {
-
-    //TODO: set "user" attribute appropriate to the data passing -- checkout ../document/user_reg.txt 
-    return new Promise((resolve, reject) => {
-        db.query(`CALL user_reg(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`, customer, (error, rows, fields) => {
-
-            if (!!error) {
-                dbFunc.connectionRelease;
-                reject(error);
-            } else {
-
-                dbFunc.connectionRelease;
-                resolve(rows);
-            }
-        });
-    });
-}
