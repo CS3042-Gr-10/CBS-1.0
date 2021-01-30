d4d8cfed51680e07777bb9796143beee34bb07fb
nipdep <nipun1deelaka@gmail.com>
2021-01-30 17:07:35 +0530

    Add: function to add fixed deposit

--- app/models/FixedDeposit.model.js
+++ app/models/FixedDeposit.model.js
@@ -1,3 +1,21 @@
 
 const db = require('../config/database');
 var dbFunc = require('../config/db-function');
+
+function addCFixedDeposit(acc) {
+
+    //TODO: set "acc" attribute appropriate to the data passing -- checkout ../document/sql_scripts/add_fd.sql 
+    return new Promise((resolve, reject) => {
+        db.query(`CALL add_fd(?,?,?,?,?,?)`, acc, (error, rows, fields) => {
+
+            if (!!error) {
+                dbFunc.connectionRelease;
+                reject(error);
+            } else {
+
+                dbFunc.connectionRelease;
+                resolve(rows);
+            }
+        });
+    });
+}
