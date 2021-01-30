9475ede7ed09b61e9c1b99e888d19cc542bd1de2
nipdep <nipun1deelaka@gmail.com>
2021-01-30 17:38:29 +0530

    Add: trasnfer money function

--- app/models/Account.model.js
+++ app/models/Account.model.js
@@ -5,7 +5,8 @@ var dbFunc = require('../config/db-function');
 var AccountModel = {
     addAccount,
     depositMoneySvAcc,
-    withdrawSvAcc
+    withdrawSvAcc,
+    transferMoney
 }
 
 function addAccount(acc) {
@@ -49,6 +50,24 @@ function withdrawSvAcc(deposit) {
     //TODO: set "deposit" attribute appropriate to the data passing -- checkout ../document/sql_scripts/withdraw_sv_acc.sql 
     return new Promise((resolve, reject) => {
         db.query(`CALL withdraw_sv_acc(?,?,?,?,?)`, deposit, (error, rows, fields) => {
+
+            if (!!error) {
+                dbFunc.connectionRelease;
+                reject(error);
+            } else {
+
+                dbFunc.connectionRelease;
+                resolve(rows);
+            }
+        });
+    });
+}
+
+function transferMoney(slip) {
+
+    //TODO: set "deposit" attribute appropriate to the data passing -- checkout ../document/sql_scripts/transfer_mn.sql 
+    return new Promise((resolve, reject) => {
+        db.query(`CALL transfer_mn(?,?,?,?,?)`, slip, (error, rows, fields) => {
 
             if (!!error) {
                 dbFunc.connectionRelease;
