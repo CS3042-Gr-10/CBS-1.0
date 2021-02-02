9a7193f3a04638a0a81ab43db58fdda2c487be9f
nipdep <nipun1deelaka@gmail.com>
2021-02-02 19:39:56 +0530

    Update: Dropdowm get list of branches from database #30

--- app/services/Dropdown.service.js
+++ app/services/Dropdown.service.js
@@ -3,12 +3,28 @@ const db = require('../../config/database');
 var dbFunc = require('../../config/db-function');
 
 const DropdownService = {
-    getBranches
+    getBranches,
+    getPosts
 }
 
 function getBranches() {
     return new Promise((resolve, reject) => {
         db.query('SELECT branch_id, branch_name from Branch', (error, rows, fields) => {
+            if (!!error) {
+                dbFunc.connectionRelease;
+                reject(false);
+            } else {
+
+                dbFunc.connectionRelease;
+                resolve(rows[0]);
+            }
+        });
+    });
+}
+
+function getPosts() {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT post_id, post_name from Post', (error, rows, fields) => {
             if (!!error) {
                 dbFunc.connectionRelease;
                 reject(false);
