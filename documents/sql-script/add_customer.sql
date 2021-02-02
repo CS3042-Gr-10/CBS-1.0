6ff628c201c273cca7125c416c39f63c4c9c279d
nipdep <nipun1deelaka@gmail.com>
2021-02-02 19:40:20 +0530

    Edit: procedures

--- documents/sql-script/add_customer.sql
+++ documents/sql-script/add_customer.sql
@@ -5,7 +5,6 @@ CREATE DEFINER=`u05dacvdhduk0jzi`@`%` PROCEDURE `add_customer`(
 	IN last_name varchar(100),
 	IN name_with_init varchar(100), 
 	IN dob date,
-	IN created_date date, 
 	IN NIC varchar(20),
 	IN gender varchar(25),
 	IN house_no varchar(25), 
@@ -29,15 +28,15 @@ BEGIN
     
     START transaction;
     
-        insert into User (user_type, username, password, acc_level)
-        value ("A", username, password, acc_level);
-        
-        insert into AccountOwner (owner_id, owner_type)
-        values (last_insert_id(), "C");
-        
-        insert into Customer (customer_id ,first_name, last_name, name_with_init, dob, created_date, NIC, gender, house_no, street, city, postal_code, contact_primary, contact_secondary)
-        values (last_insert_id(), first_name, last_name, name_with_init, dob, created_date, NIC, gender, house_no, street, city, postal_code, contact_primary, contact_secondary);
-        
+	insert into User (user_type, username, password, acc_level)
+    value ("A", username, password, acc_level);
+    
+    insert into AccountOwner (owner_id, owner_type)
+    values (last_insert_id(), "C");
+    
+	insert into Customer (customer_id ,first_name, last_name, name_with_init, dob, created_date, NIC, gender, house_no, street, city, postal_code, contact_primary, contact_secondary)
+	values (last_insert_id(), first_name, last_name, name_with_init, dob, curdate(), NIC, gender, house_no, street, city, postal_code, contact_primary, contact_secondary);
+    
     COMMIT WORK;
     
 END
--- documents/sql-script/add_emp.sql
+++ documents/sql-script/add_emp.sql
@@ -6,7 +6,6 @@ CREATE DEFINER=`u05dacvdhduk0jzi`@`%` PROCEDURE `add_emp`(
 	IN last_name varchar(100), 
 	IN name_with_init varchar(100), 
 	IN dob date, 
-	IN created_date date, 
 	IN postal_code int(8), 
 	IN contact_No int(10), 
 	IN NIC varchar(20), 
@@ -31,12 +30,12 @@ BEGIN
     
     START TRANSACTION;
     
-        insert into User (user_type, username, password, acc_level)
-        value ("E", username, password, acc_level);
-        
-        insert into Employee (emp_id, first_name, last_name, name_with_init, dob, created_date, postal_code, contact_No, NIC, brach_id, gender, house_no, street, city, post_id)
-        value (last_insert_id(), first_name, last_name, name_with_init, dob, created_date, postal_code, contact_No, NIC, brach_id, gender, house_no, street, city, post_id);
-        
+    insert into User (user_type, username, password, acc_level)
+    value ("E", username, password, acc_level);
+    
+    insert into Employee (emp_id, first_name, last_name, name_with_init, dob, created_date, postal_code, contact_No, NIC, brach_id, gender, house_no, street, city, post_id)
+    value (last_insert_id(), first_name, last_name, name_with_init, dob, curdate(), postal_code, contact_No, NIC, brach_id, gender, house_no, street, city, post_id);
+    
     COMMIT WORK;
 
 END
