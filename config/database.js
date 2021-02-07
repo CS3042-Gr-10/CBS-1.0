10a22fb29ce91f467b937db52ba81bdbd0097354
nipdep <nipun1deelaka@gmail.com>
2021-02-07 11:39:35 +0530

    Update: database.js

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 module.exports = mysql.createPool({
     user: 'dev',
     host: '34.101.117.36',
-    database: 'cbs-1-db',
+    database: 'main_db',
     port: 3306,
     password: 'NDUCI@cse3042',
 })
