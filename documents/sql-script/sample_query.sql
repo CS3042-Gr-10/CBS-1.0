3743dc31537c21beb89efec99f2e1e03dc7a01be
nipdep <nipun1deelaka@gmail.com>
2021-02-15 13:02:42 +0530

    Complete: transfer money #50

--- documents/sql-script/sample_query.sql
+++ documents/sql-script/sample_query.sql
@@ -17,11 +17,17 @@ call add_emp('Devindesilva',
 
 call add_customer('DevinDe', '551233240c', 'devindesilva123@gmail.com', 'Devin', 'De Silva', 'Devin De Silva','2001-01-30',  '982930130v', 'male', '124/2', 'Salgas Mawatha', 'Kottawa', 10320, 0713452365, 0775673546, "CUSTOMER");
 
-call add_saving_account(1, 200.0, 6, 2);
+call add_saving_account(1, 600.0, 6, 2);
 
 call add_current_account(1, 12100.0, 17);
 
-call deposit_mn_sv_acc(200, 4, 12);
+call deposit_mn_sv_acc(200, 4, 2);
+
+call withdraw_mn_sv_acc(2, 3, 10);
+
+call transfer_mn(8, 100, 4, 2);
+
+call add_fd_acc(6, 2, 2, 1, 10000, 1);
 
 select trans_id, trans_type, acc_id, deposit_type, amount, date from (select trans_id, deposit_type, acc_id from Deposit where acc_id in (select acc_id from Account where user = 6)) as P natural join (select * from Transaction where is_deleted = 0) as Q;
 
