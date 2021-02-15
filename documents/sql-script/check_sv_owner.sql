aa79321c8f44a329b58cc6347499cccc5cec7b20
nipdep <nipun1deelaka@gmail.com>
2021-02-15 12:40:51 +0530

    Add: fiexed deposit started #49

---
+++ documents/sql-script/check_sv_owner.sql
@@ -0,0 +1,15 @@
+CREATE DEFINER=`dev`@`%` FUNCTION `check_sv_owner`(
+	acc_id_d int(32),
+    user_id_d int(11)
+) RETURNS int(11)
+BEGIN
+	declare result int(1);
+    declare act_user int(11);
+    select user into act_user from account where acc_id = acc_id_d;
+    if act_user = user_id_d then
+		set result = 1;
+	else 
+		set result = 0;
+	end if;
+	RETURN result;
+END
