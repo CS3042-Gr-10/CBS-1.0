745f0b453dac202efcb3c68fe586f54422508344
nipdep <nipun1deelaka@gmail.com>
2021-02-17 12:21:14 +0530

    Update: Account.model typo

--- app/models/Account.model.js
+++ app/models/Account.model.js
@@ -68,7 +68,7 @@ function withdrawSvAcc(deposit) {
 
     //TODO: set "deposit" attribute appropriate to the data passing -- checkout ../document/sql_scripts/withdraw_sv_acc.sql 
     return new Promise((resolve, reject) => {
-        db.query(`CALL withdraw_sv_acc(?,?,?,?,?)`, deposit, (error, rows, fields) => {
+        db.query(`CALL withdraw_mn_sv_acc(?,?,?,?,?)`, deposit, (error, rows, fields) => {
 
             if (!!error) {
                 dbFunc.connectionRelease;
