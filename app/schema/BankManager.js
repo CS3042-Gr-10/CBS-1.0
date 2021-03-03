d81cf49c2a18b1ca78c2d42a99ff6b3e1c65d89f
DevinDeSilva <devin.18@cse.mrt.ac.lk>
2021-03-03 10:22:48 +0530

    report generation error correction

--- app/schema/BankManager.js
+++ app/schema/BankManager.js
@@ -5,6 +5,7 @@ const stdLoanApprovalInfo = Joi.object().options({ abortEarly: false }).keys({
 });
 
 const TransactionReportInfo = Joi.object().options({abortEarly: false}).keys({
+    branch:Joi.string().required().max(15).label("Branch"),
     start_date:Joi.date().iso().label("Start Date").required(),
     end_date:Joi.date().iso().min(Joi.ref('start_date')).message("Start Date must be earlier than End Date").label("End Date").required()
 });
